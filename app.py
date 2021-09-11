7f76c324885036a31012bb4679f09adfe144a5d3
jmw1973 <31631819+jmw1973@users.noreply.github.com>
2021-09-11 15:06:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,8 @@ def hello():
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
     html = "<h3>Hello {name}!</h3>" \
-           "<b>We are running the latest container image!</b><br/>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
+           "<b>Your Hostname:</b> {hostname}<br/>" \
+           "<b>Visits so far:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
